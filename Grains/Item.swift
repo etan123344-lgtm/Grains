49ae194dfd49fdb49e3d476b669022be6775936c
mayihavecrumbofcoochieplz <ezhou24@wisc.edu>
2026-02-24 15:22:47 -0600

    Initial Commit

---
+++ Grains/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Grains
+//
+//  Created by Ethan Zhou on 2/24/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
